5c4493630fba9826c9e87ea7bca10cc8ec73febf
Laura Pekkanen <K8966@studrent.jamk.fi>
2017-02-23 11:28:12 +0200

    i tried

---
+++ Teht02_muunnin/MainPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Teht02_muunnin
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        double kurssi = 5.94573;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void eurotButton(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void markatButton(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+    }
+}
